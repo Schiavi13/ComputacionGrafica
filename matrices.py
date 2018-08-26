5ecbb9801b5f78e1fe363f3ae1b7d83ebd2a7018
Luis Martínez <lufmartinezmu@gmail.com>
2018-08-25 20:05:40 -0500

    Add files via upload

--- matrices.py
+++ matrices.py
@@ -1,6 +1,6 @@
 
 #suma = [x[:] for x in matriz1]
-#Retorna la columna n de la matriz
+#Recibe una lista de listas de numeros (matriz) y un numero n y retorna la columna numero n de la matriz
 import math
 def columna(matriz,n):  
     if (n>=0):
@@ -14,14 +14,14 @@ def columna(matriz,n):
     else:
         return "La dimension debe ser positiva"
 
-#Retorna una matriz identidad de dimension nxn
+#Recibe un numero n y retorna una matriz identidad de dimension nxn
 def matrizIdentidad(n):
     matriz = [[0 for x in range (n)] for y in range (n)] #inicializa la matriz con solo 0
     for i in range(0,n):
         matriz[i][i] = 1
     return matriz
 
-#Retorna el producto punto entre dos matrices nxm . mxn
+#Recibe dos listas de listas de numeros (matrices) y retorna el producto punto entre dos matrices nxm . mxn
 def productoPunto(matriz1,matriz2):
     sum = 0
     for i in range(0,len(matriz1)): #verifica que m1 = nxm y m2 = mxn
@@ -32,26 +32,27 @@ def productoPunto(matriz1,matriz2):
             return "El numero de filas de la primer matriz debe ser igual al numero de columnas de la segunda matriz y viceversa"
     return sum
 
-#retorna una matriz con todos los valores en 0 de la misma dimension que matriz1
+#Recibe una lista de listas de numeros (matriz) y retorna una matriz con todos los valores en 0 de la misma dimension que matriz1
 def crearMatrizVacia(matriz1):
     suma = [x[:] for x in matriz1]
     return suma
 
-#Evalua si dos matrices tienen el mismo numero de filas
+#Recibe dos listas de listas de numeros (matrices) y retorna un booleano: True si tienen la misma cantidad de filas y False si es el
+#caso contrario
 def igualdadFilas(matriz1,matriz2): 
     if (len(matriz1)==len(matriz2)): 
         return True
     else:
         return False
 
-#Funcion que crea una lista aparte por cada fila y con cada elemento de la fila de una matriz
+#Recibe una lista de listas de numeros (matriz) y retorna cada una de las filas de la matriz por separado
 def crearFila(matriz):  
     sublista = []
     for j in range (0,len(matriz)):
         sublista.append(matriz[j])
     return sublista
 
-#funcion que suma dos matrices
+#Recibe dos listas de listas de numeros (matrices) y retorna la suma de las dos matrices
 def sumarMatrices(matriz1,matriz2):
     suma = crearMatrizVacia(matriz1)
     if(igualdadFilas(matriz1,matriz2)): #evalua si tienen mismo numero de columnas
@@ -69,21 +70,21 @@ def sumarMatrices(matriz1,matriz2):
     else:
         return "Ambas matrices deben tener el mismo numero de filas"
 
-#Retorna la norma de un vector unidimensional
+#Recibe una lista de numeros (vector) y retorna la norma del vector
 def normaVector(vector):
     sum = 0
     for i in range(0,len(vector)):
         sum = sum + math.pow(vector[i],2)
     return math.sqrt(sum)
 
-#Retorna verdadero si los vectores dados tienen la misma dimension
+#Recibe dos listas de numeros (vectores) y retorna un booleano: True si tienen la misma dimension y False en el caso contrario
 def igualdadDimensionVector(vector1,vector2):
     if len(vector1) == len(vector2):
         return True
     else:
         return False
 
-#Retorna el vector resultante entre dos vectores
+#Recibe dos listas de numeros (vectores) y retorna el vector resultante entre los vectores
 def vectorResultante(vector1,vector2):
     resultante = []
     if igualdadDimensionVector(vector1,vector2):
@@ -115,4 +116,16 @@ def perimetroTriangulo(puntos):
 def anguloVectores(vector1,vector2):
     if len(vector1) == len(vector2):
         return math.degrees(math.acos((productoPuntoVectores(vector1,vector2)/(normaVector(vector1)*normaVector(vector2)))))
+
+#Recibe dos numeros que representan los catetos de un triangulo rectangulo y retorna la hipotenusa
+def calcularHipotenusa(c_ad,c_op):
+    return math.sqrt(math.pow(c_ad,2)+math.pow(c_op,2))
+
+#Recibe dos numeros que representan los catetos de un triangulo rectangulo y retorna los angulos internos del triangulo
+def calcularAngulosTRect(c_ad,c_op):
+    angulos = [90]
+    hipotenusa = calcularHipotenusa(c_ad,c_op)
+    angulos.append(round(math.degrees(math.asin(c_op/hipotenusa)),2))
+    angulos.append(round(math.degrees(math.asin(c_ad/hipotenusa)),2))
+    return angulos
 
